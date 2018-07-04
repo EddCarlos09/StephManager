6e222118d7e6349e317fd0d436858b4927ceb785
EddCarlos09 <lackke.141727@gmail.com>
2018-07-04 11:08:36 -0500

    Comentarios

--- StepthManager/CreativaSL.Dll.StepthManager.Global/ReporteMaterialesProduccion.cs
+++ StepthManager/CreativaSL.Dll.StepthManager.Global/ReporteMaterialesProduccion.cs
@@ -8,12 +8,34 @@ namespace CreativaSL.Dll.StephManager.Global
 {
     public class ReporteMaterialesProduccion
     {
+        /// <summary>
+        /// Identificador de la sucursal a la que pertenece el Empleado
+        /// </summary>
         public string IDSucursal { get; set; }
+        /// <summary>
+        /// Nombre de la sucursal
+        /// </summary>
         public string NombreSucursal { get; set; }
+        /// <summary>
+        /// Identificador del empleado al que pertenece el material
+        /// </summary>
         public string IDEmpleado { get; set; }
+        /// <summary>
+        /// Nombre del empleado
+        /// </summary>
         public string NombreEmpleado { get; set; }
+        /// <summary>
+        /// Código de barras del producto 
+        /// </summary>
         public string ClaveProducto { get; set; }
+        /// <summary>
+        /// Nombre del producto
+        /// </summary>
         public string NombreProducto { get; set; }
-        public int Cantidad { get; set; }        
+        /// <summary>
+        /// Cantidad de producto en producción
+        /// </summary>
+        public int Cantidad { get; set; }
+        
     }
 }
