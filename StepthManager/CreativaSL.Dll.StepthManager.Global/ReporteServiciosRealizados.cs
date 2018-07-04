d756b5fc0beb4c64164796e034b60648a8b78449
EddCarlos09 <lackke.141727@gmail.com>
2018-07-04 11:16:06 -0500

    Comentarios en propiedades

--- StepthManager/CreativaSL.Dll.StepthManager.Global/ReporteServiciosRealizados.cs
+++ StepthManager/CreativaSL.Dll.StepthManager.Global/ReporteServiciosRealizados.cs
@@ -8,12 +8,33 @@ namespace CreativaSL.Dll.StephManager.Global
 {
     public class ReporteServiciosRealizados
     {
+        /// <summary>
+        /// Identificador de la sucursal en la que se realizan los servicios
+        /// </summary>
         public string IDSucursal { get; set; }
+        /// <summary>
+        /// Nombre de la sucursal
+        /// </summary>
         public string NombreSucursal { get; set; }
+        /// <summary>
+        /// Identificador del empleado que realizó los servicios
+        /// </summary>
         public string IDEmpleado { get; set; }
+        /// <summary>
+        /// Nombre del empleado
+        /// </summary>
         public string NombreEmpleado { get; set; }
+        /// <summary>
+        /// Identificador del servicio realizado
+        /// </summary>
         public string IDServicio { get; set; }
+        /// <summary>
+        /// Nombre del servicio realizado
+        /// </summary>
         public string NombreServicio { get; set; }
+        /// <summary>
+        /// Cantidad de servicios realizados
+        /// </summary>
         public int Cantidad { get; set; }
     }
 }
